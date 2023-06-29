41b9d6cf67a2cdcc48c440d7f3aa1d038b1d79f2
Ramon A. Rivera <ramonrivera550@gmail.com>
2023-06-29 01:28:17 -0400

    I Heart JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import HeartLogo from './heart.svg';
 import './App.css';
 
-function App() {
+const message = 'cool cud';
+
+const Heart = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='heart'>
+        <img className='heart-img' src={HeartLogo} alt='heart' />
+        <p className='heart-message'>{props.msg}</p>
     </div>
-  );
+  )
+};
+
+function App() {
+  return <Heart msg={message} />;
 }
 
 export default App;
