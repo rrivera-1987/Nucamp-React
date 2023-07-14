4c18fa5ce0401a0aa920adc287b5d3c30090762c
Ramon A. Rivera <ramonrivera550@gmail.com>
2023-07-14 12:05:50 -0400

    React Week 3 Task 2

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -4,6 +4,7 @@ import { Formik, Field, Form } from 'formik';
 
 const CommentForm = ({ campsiteId }) => {
     const [modalOpen, setModalOpen] = useState(false);
+    
     const handleSubmit = (values) => {
         const comment = {
             campsiteId: parseInt(campsiteId),
@@ -24,7 +25,53 @@ const CommentForm = ({ campsiteId }) => {
                 <ModalHeader toggle={() => setModalOpen(false)}>
                     Add Comment
                 </ModalHeader>
-                    <ModalBody>campsite: {campsiteId}</ModalBody>
+                <ModalBody>
+                    <Formik 
+                        initialValues={{ 
+                            rating: undefined, 
+                            author: '', 
+                            commentText: ''}}
+                        onSubmit={handleSubmit}
+                    >
+                        <Form>
+                            <FormGroup>
+                                <Label htmlFor='rating'>Rating</Label>
+                                <Field
+                                    name='rating'
+                                    as='select'
+                                    className='form-control'
+                                >
+                                    <option>Select...</option>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Field>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor='author'>Your Name</Label>
+                                <Field
+                                    name='author'
+                                    placeholder='Your Name'
+                                    className='form-control'
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor='commentText'>Comment</Label>
+                                <Field
+                                    name='commentText'
+                                    as='textarea'
+                                    rows='12'
+                                    className='form-control'
+                                />
+                            </FormGroup>
+                            <Button type='submit' color='primary'>
+                                Submit
+                            </Button>
+                        </Form>
+                    </Formik>
+                </ModalBody>
             </Modal>
         </>
     );
