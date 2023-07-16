2745fc3f1d348a49eee072782821ee46f4486330
Ramon A. Rivera <ramonrivera550@gmail.com>
2023-07-16 16:53:18 -0400

    React Week 3

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap';
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { validateCommentForm } from '../../utils/validateCommentForm';
 
 const CommentForm = ({ campsiteId }) => {
     const [modalOpen, setModalOpen] = useState(false);
@@ -26,12 +27,13 @@ const CommentForm = ({ campsiteId }) => {
                     Add Comment
                 </ModalHeader>
                 <ModalBody>
-                    <Formik 
+                    <Formik  
                         initialValues={{ 
                             rating: undefined, 
                             author: '', 
                             commentText: ''}}
                         onSubmit={handleSubmit}
+                        validate={validateCommentForm}
                     >
                         <Form>
                             <FormGroup>
@@ -48,6 +50,9 @@ const CommentForm = ({ campsiteId }) => {
                                     <option>4</option>
                                     <option>5</option>
                                 </Field>
+                                <ErrorMessage name='rating'>
+                                    {(msg) => <p className='text-danger'>{msg}</p>}
+                                </ErrorMessage>
                             </FormGroup>
                             <FormGroup>
                                 <Label htmlFor='author'>Your Name</Label>
@@ -56,6 +61,9 @@ const CommentForm = ({ campsiteId }) => {
                                     placeholder='Your Name'
                                     className='form-control'
                                 />
+                                <ErrorMessage name='author'>
+                                    {(msg) => <p className='text-danger'>{msg}</p>}
+                                </ErrorMessage>
                             </FormGroup>
                             <FormGroup>
                                 <Label htmlFor='commentText'>Comment</Label>
