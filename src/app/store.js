48d5e53fff1ddf00350644da477dc5dba7d23aeb
Ramon A. Rivera <ramonrivera550@gmail.com>
2023-07-20 11:45:06 -0400

    Add middleware

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer } from '../features/promotions/PromotionsSlice';
+import logger from 'redux-logger';
 
 export const store = configureStore({
     reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
         comments: commentsReducer,
         partners: partnersReducer,
         promotions: promotionsReducer
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
