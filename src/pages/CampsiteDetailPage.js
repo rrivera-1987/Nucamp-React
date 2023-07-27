91b3f6b1ad87db2663da16f1c96cfd4e06b2c268
Ramon A. Rivera <ramonrivera550@gmail.com>
2023-07-26 23:52:00 -0400

    Error and Loading

--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -1,10 +1,12 @@
+import { useSelector } from 'react-redux';
 import { Container, Row } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 import { selectCampsiteById } from '../features/campsites/campsitesSlice';
 import CampsiteDetails from '../features/campsites/CampsiteDetails';
 import CommentsList from '../features/comments/CommentsList';
 import SubHeader from '../components/SubHeader';
-import { useSelector } from 'react-redux';
+import Error from '../components/Error';
+import Loading from '../components/Loading';
 
 const CampsiteDetailPage = () => {
     const { campsiteId } = useParams();
@@ -12,13 +14,27 @@ const CampsiteDetailPage = () => {
     const campsite = useSelector(selectCampsiteById(campsiteId)); 
     console.log('campsite:', campsite);
 
-    return (
-        <Container>
-            <SubHeader current={campsite.name} detail={true} />
-            <Row>
+    const isLoading = useSelector((state) => state.campsites.isLoading);
+    const errMsg = useSelector((state) => state.campsites.errMsg);
+    let content = null;
+
+    if (isLoading) {
+        content = <Loading />;
+    } else if (errMsg) {
+        content = <Error errMsg={errMsg} />;
+    } else {
+        content = (
+            <>
                 <CampsiteDetails campsite={campsite} />
                 <CommentsList campsiteId={campsiteId} />
-            </Row>
+            </>
+        );
+    }
+
+    return (
+        <Container>
+            {campsite && <SubHeader current={campsite.name} detail={true} />}
+            <Row>{content}</Row>
         </Container>
     );
 };
